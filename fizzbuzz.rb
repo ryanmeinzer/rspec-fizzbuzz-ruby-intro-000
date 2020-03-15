0a3d65d9aa8a34fd28e26d69238a064c86b2ab61
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-03-15 21:32:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elseif int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  elseif int % 15 == 0
+  elsif int % 15 == 0
     "FizzBuzz"
   else
-    "nil" 
+    "nil"
   end
 end
